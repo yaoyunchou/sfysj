03de493b9833768cc9898ec740a0e2841efb0bb8
Administrator <Administrator@2011-20140814JZ>
2015-08-25 19:14:17 +0800

    加载问题位解决  动画不出现对应效果

--- app/sea-modules/common/indexController.js
+++ app/sea-modules/common/indexController.js
@@ -2,6 +2,7 @@ define(function(require,exports,module){
 	//require("jquery");
 	require("ag");
 	
+	
 	var $=this.jQuery,jQuery=this.jQuery;
 	
 	console.log($)
@@ -11,21 +12,18 @@ define(function(require,exports,module){
   var hjf = angular.module('myapp', []);  
   
   hjf.controller('BannerCtrl', ['$scope','$http' , function($scope,$http) {  
-  	$scope.phones = [
-		    {"name": "Nexus S",
-		     "snippet": "Fast just got faster with Nexus S.","age":"30"},
-		    {"name": "Motorola XOOM™ with Wi-Fi",
-		     "snippet": "The Next, Next Generation tablet.","age":"20"},
-		    {"name": "MOTOROLA XOOM™",
-		     "snippet": "The Next, Next Generation tablet.","age":"22"}
-		  ];        
+  	
 	 $http.get('../sea-modules/php/postandget.php?url=https://www.p2pdi.com/appinterface/indexbanner.json').success(function(data) {
 		    
 		   $scope.hjfimgs = data.result.object;
-		    console.log( $scope.hjfimgs);
+		    
 		  });
-  
-		   $scope.orderProp = 'age';
-  }]);  
+    
+		   //$scope.orderProp = 'age';
+  }],function(){
+  	var b = require('common/base')
+  	
+  	
+  });
  
 });
