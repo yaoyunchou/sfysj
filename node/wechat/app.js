ec4283c2182342fef14f0da9dba06db59818c15b
yao <303630573@qq.com>
2016-11-01 08:22:11 +0800

    wechat

--- node/wechat/app.js
+++ node/wechat/app.js
@@ -3,30 +3,28 @@
  */
 "use strict";
 var Koa = require('koa');
-var sha1 = require('sha1');
+var wechat = require('./wechat/g');
+var path = require('path');
+var util = require('./libs/util');
+var wechat_file = path.join(__dirname,'./config/wechat.txt');
+
 var config = {
-	wechat:{
-		appID:'wx70ae2cf92f03f04a',
-		appSecret:'11c20988538deadbc2df9ce8a0834140',
-		token:'yaoyunchou'
+	wechat: {
+		appID: 'wx70ae2cf92f03f04a',
+		appSecret: '11c20988538deadbc2df9ce8a0834140',
+		token: 'yaoyunchou',
+		getAccessToken:function(){
+			return util.readFileAsync(wechat_file);
+		},
+		saveAccessToken:function(data){
+			data = JSON.stringify(data);
+			return util.writeFileAsync(wechat_file,data);
+		}
 	}
 };
 var app = new Koa();
-app.use(function *(next){
-	console.log(this.query);
-	var token = config.wechat.token;
-	var signature = this.query.signature;
-	var timestamp = this.query.timestamp;
-	var nonce = this.query.nonce;
-	var echostr = this.query.echostr;
-	var str = [token, timestamp, nonce].sort().join('');
-	var sha = sha1(str);
-	if(sha === signature){
-		this.body = echostr + '';
-	}else {
-		this.body = 'wrong';
-	}
-});
+app.use(wechat(config.wechat));
 
 app.listen(80);
+
 console.log('Listening:80');
